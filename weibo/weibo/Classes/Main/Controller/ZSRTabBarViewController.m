d067ea8cad13c3a590e1a13511d89bec38b82a44
zsr <lpzsrong@gmail.com>
2015-12-04 23:02:06 +0800

    微博顶部-新版本特性(添加按钮)

--- weibo/weibo/Classes/Main/Controller/ZSRTabBarViewController.m
+++ weibo/weibo/Classes/Main/Controller/ZSRTabBarViewController.m
@@ -38,8 +38,22 @@
     // 2.更换系统自带的tabbar
     //    self.tabBar = [[ZSRTabBar alloc] init];
     ZSRTabBar *tabBar = [[ZSRTabBar alloc] init];
-    tabBar.delegate = self;
     [self setValue:tabBar forKeyPath:@"tabBar"];
+    /*
+     [self setValue:tabBar forKeyPath:@"tabBar"];相当于self.tabBar = tabBar;
+     [self setValue:tabBar forKeyPath:@"tabBar"];这行代码过后，tabBar的delegate就是HWTabBarViewController
+     说明，不用再设置tabBar.delegate = self;
+     */
+    
+    /*
+     1.如果tabBar设置完delegate后，再执行下面代码修改delegate，就会报错
+     tabBar.delegate = self;
+     
+     2.如果再次修改tabBar的delegate属性，就会报下面的错误
+     错误信息：Changing the delegate of a tab bar managed by a tab bar controller is not allowed.
+     错误意思：不允许修改TabBar的delegate属性(这个TabBar是被TabBarViewController所管理的)
+     */
+
 }
 
 /**
