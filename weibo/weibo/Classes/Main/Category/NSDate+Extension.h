4243d049c515986363cfe64908d03824286e6e54
zsr <lpzsrong@gmail.com>
2015-12-11 00:22:10 +0800

    微博首页-日期处理

---
+++ weibo/weibo/Classes/Main/Category/NSDate+Extension.h
@@ -0,0 +1,24 @@
+//
+//  NSDate+Extension.h
+//  weibo
+//
+//  Created by hp on 15/12/11.
+//  Copyright © 2015年 hp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (Extension)
+/**
+ *  判断某个时间是否为今年
+ */
+- (BOOL)isThisYear;
+/**
+ *  判断某个时间是否为昨天
+ */
+- (BOOL)isYesterday;
+/**
+ *  判断某个时间是否为今天
+ */
+- (BOOL)isToday;
+@end
---
+++ weibo/weibo/Classes/Main/Category/NSDate+Extension.m
@@ -0,0 +1,70 @@
+//
+//  NSDate+Extension.m
+//  weibo
+//
+//  Created by hp on 15/12/11.
+//  Copyright © 2015年 hp. All rights reserved.
+//
+
+#import "NSDate+Extension.h"
+
+
+@implementation NSDate (Extension)
+
+/**
+ *  判断某个时间是否为今年
+ */
+- (BOOL)isThisYear
+{
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    // 获得某个时间的年月日时分秒
+    NSDateComponents *dateCmps = [calendar components:NSCalendarUnitYear fromDate:self];
+    NSDateComponents *nowCmps = [calendar components:NSCalendarUnitYear fromDate:[NSDate date]];
+    return dateCmps.year == nowCmps.year;
+}
+
+/**
+ *  判断某个时间是否为昨天
+ */
+- (BOOL)isYesterday
+{
+    NSDate *now = [NSDate date];
+    
+    // date ==  2015-04-30 10:05:28 --> 2015-04-30 00:00:00
+    // now == 2015-05-01 09:22:10 --> 2014-05-01 00:00:00
+    NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
+    fmt.dateFormat = @"yyyy-MM-dd";
+    
+    // 2015-04-30
+    NSString *dateStr = [fmt stringFromDate:self];
+    // 2015-10-18
+    NSString *nowStr = [fmt stringFromDate:now];
+    
+    // 2015-10-30 00:00:00
+    NSDate *date = [fmt dateFromString:dateStr];
+    // 2015-10-18 00:00:00
+    now = [fmt dateFromString:nowStr];
+    
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    
+    NSCalendarUnit unit = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay;
+    NSDateComponents *cmps = [calendar components:unit fromDate:date toDate:now options:0];
+    
+    return cmps.year == 0 && cmps.month == 0 && cmps.day == 1;
+}
+
+/**
+ *  判断某个时间是否为今天
+ */
+- (BOOL)isToday
+{
+    NSDate *now = [NSDate date];
+    NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
+    fmt.dateFormat = @"yyyy-MM-dd";
+    
+    NSString *dateStr = [fmt stringFromDate:self];
+    NSString *nowStr = [fmt stringFromDate:now];
+    
+    return [dateStr isEqualToString:nowStr];
+}
+@end
